437aad43fa88951a6fd16d7bc13d2b443e6f6ef8
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-29 15:44:48 +0100

    progression

--- graph/node_.js
+++ graph/node_.js
@@ -1,13 +1,34 @@
 // This file is named "node_.js",
 // because naming it "node.js" causes problem with "npx mocha" command
 export default class Node {
-  constructor(x, y, neighbors = []) {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.neighbors = neighbors;
+    this.neighbors = [];
   }
 
   addNeighbor(node) {
     this.neighbors.push(node);
+  }
+
+  getNodesAsSet() {
+    const mySet = new Set();
+    const queue = [];
+
+    queue.push(this);
+    console.log("Get ready for this!");
+    console.log(this);
+
+    while (queue.length > 0) {
+      const currentNode = queue.shift();
+      console.log(currentNode.neighbors);
+      mySet.add(currentNode); // using a Set avoids duplicates
+
+      for (const node of currentNode.neighbors) {
+        if (mySet.has(node) == false) queue.push(node);
+      }
+    }
+
+    return mySet;
   }
 }
--- sketch.js
+++ sketch.js
@@ -8,20 +8,26 @@ async function loadModules() {
   // Now you can use Node
   const n1 = new Node(300, 50);
   const n2 = new Node(200, 200);
+  const n3 = new Node(400, 500);
 
   n1.addNeighbor(n2);
+  n1.addNeighbor(n3);
 
   console.log(n1);
 
   nodes.push(n1);
   nodes.push(n2);
+  nodes.push(n3);
 }
 
 // setup() is called once when the sketch begins running
 async function setup() {
   await loadModules();
-
   createCanvas(1400, 600);
+
+  const nodeA = nodes[0];
+  console.log(nodeA.neighbors);
+  const mySet = nodeA.getNodesAsSet();
 }
 
 // draw() is run repeatedly approx. 60 times per second
@@ -31,7 +37,11 @@ function draw() {
   //circle in the center with a width of 50
   // circle(200, 200, 50);
 
-  for (n of nodes) {
+  const nodeA = nodes[0];
+  const mySet = nodeA.getNodesAsSet();
+  console.log(mySet);
+
+  for (n of mySet) {
     circle(n.x, n.y, 50);
   }
 
