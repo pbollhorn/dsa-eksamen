1ed81d5801d1ed408b0f539346b1ad89de9fc0b3
Peter Bollhorn <pbollhorn@gmail.com>
2025-12-01 14:07:47 +0100

    bfs runs now

--- graph/graph.js
+++ graph/graph.js
@@ -14,13 +14,25 @@ export default class Graph {
 
   // My implementation of BFS
   bfs(startNode, goalNode) {
-    console.log("Hello from BFS!!!");
-    console.log(startNode);
-    console.log(goalNode);
+    console.log("BFS stated");
 
     const queue = []; // JavaScript array used as a queue
     const visited = new Set();
 
     queue.push(startNode);
+
+    while (queue.length > 0) {
+      const node = queue.shift();
+      console.log(node);
+      if (!visited.has(node)) {
+        if (node === goalNode) {
+          console.log("Goal Node Found!");
+          return;
+        } else {
+          queue.push(...node.links);
+          visited.add(node);
+        }
+      }
+    }
   }
 }
