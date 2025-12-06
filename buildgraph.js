5580beedb373dc51d18f9463351d6a9bd96dd0dd
Peter Bollhorn <pbollhorn@gmail.com>
2025-12-06 22:58:53 +0100

    progression

--- buildgraph.js
+++ buildgraph.js
@@ -4,49 +4,42 @@ export default function buildGraph() {
   const graph = new Graph();
 
   // Put unconnected nodes ind i grafren
-  graph.addNode("A", 43, 43);
-  graph.addNode("B", 43, 140);
-  graph.addNode("C", 43, 235);
-  graph.addNode("D", 43, 390);
-  graph.addNode("E", 43, 480);
-  graph.addNode("F", 140, 43);
-  graph.addNode("G", 140, 140);
-  graph.addNode("H", 140, 235);
-  graph.addNode("I", 190, 390);
-  graph.addNode("J", 190, 480);
-  graph.addNode("K", 235, 43);
-  graph.addNode("L", 235, 140);
-  graph.addNode("M", 235, 235);
-  graph.addNode("N", 335, 43);
-  graph.addNode("O", 335, 140);
-  graph.addNode("P", 335, 235);
-  graph.addNode("Q", 335, 390);
-  graph.addNode("R", 335, 480);
-  graph.addNode("S", 430, 43);
-  graph.addNode("T", 430, 235);
-  graph.addNode("U", 430, 390);
-  graph.addNode("V", 430, 480);
-  graph.addNode("X", 525, 43);
-  graph.addNode("Y", 525, 235);
-  graph.addNode("Z", 525, 390);
-  graph.addNode("a", 625, 43);
-  graph.addNode("a", 625, 43);
-  graph.addNode("b", 625, 140);
-  graph.addNode("c", 625, 235);
-  graph.addNode("d", 625, 390);
-  graph.addNode("e", 625, 480);
-  graph.addNode("f", 720, 43);
-  graph.addNode("g", 720, 140);
-  graph.addNode("h", 720, 235);
-  graph.addNode("i", 720, 390);
-  graph.addNode("j", 720, 480);
-
-
-  // graph.addNode("G", 720, 43);
-  // graph.addNode("H", 140, 43);
-  // graph.addNode("I", 140, 140);
-  
-  // graph.addNode("K", 675, 270);
+  graph.addNode("a", 43, 43);
+  graph.addNode("b", 43, 140);
+  graph.addNode("c", 43, 235);
+  graph.addNode("d", 43, 390);
+  graph.addNode("e", 43, 480);
+  graph.addNode("f", 140, 43);
+  graph.addNode("g", 140, 140);
+  graph.addNode("h", 140, 235);
+  graph.addNode("i", 190, 390);
+  graph.addNode("j", 190, 480);
+  graph.addNode("k", 235, 43);
+  graph.addNode("l", 235, 140);
+  graph.addNode("m", 235, 235);
+  graph.addNode("n", 335, 43);
+  graph.addNode("o", 335, 140);
+  graph.addNode("p", 335, 235);
+  graph.addNode("q", 335, 390);
+  graph.addNode("r", 335, 480);
+  graph.addNode("s", 430, 43);
+  graph.addNode("t", 430, 235);
+  graph.addNode("u", 430, 390);
+  graph.addNode("v", 430, 480);
+  graph.addNode("w", 525, 43);
+  graph.addNode("x", 525, 235);
+  graph.addNode("y", 525, 390);
+  graph.addNode("z", 625, 43);
+  graph.addNode("æ", 625, 43);
+  graph.addNode("ø", 625, 140);
+  graph.addNode("å", 625, 235);
+  graph.addNode("α", 625, 390);
+  graph.addNode("β", 625, 480);
+  graph.addNode("γ", 720, 43);
+  graph.addNode("δ", 720, 140);
+  graph.addNode("ε", 720, 235);
+  graph.addNode("ζ", 720, 390);
+  graph.addNode("η", 720, 480);
 
   // Lav links imellem grafens nodes
   // graph.addLink("A", "B");
