ea2bc2477629a41aaeb25b3a74ec2abb883844f1
Peter Bollhorn <pbollhorn@gmail.com>
2025-12-07 12:10:11 +0100

    created draw.js

---
+++ js/draw.js
@@ -0,0 +1,4 @@
+const NODE_RADIUS = 15;
+const CUSTOM_RED = "hsl(360, 100%, 60%)";
+const CUSTOM_GREEN = "hsl(120, 100%, 35%)";
+const CUSTOM_LIGHT_GREEN = "hsl(120, 100%, 75%)";
--- sketch.js
+++ sketch.js
@@ -1,8 +1,3 @@
-const NODE_RADIUS = 15;
-const CUSTOM_RED = "hsl(360, 100%, 60%)";
-const CUSTOM_GREEN = "hsl(120, 100%, 35%)";
-const CUSTOM_LIGHT_GREEN = "hsl(120, 100%, 75%)";
-
 let graph;
 let bgImg;
 let PriorityQueue;
