baebcecedad97e5917cc4518dbdfd869b1c781a9
Peter Bollhorn <pbollhorn@gmail.com>
2025-12-07 12:03:22 +0100

    created buttons.js

---
+++ js/buttons.js
@@ -0,0 +1,56 @@
+let DISPLAY_BACKGROUND = true;
+let DISPLAY_F_G_PREV = true;
+let DISPLAY_COORDINATES = false;
+let DISPLAY_WEIGHTS = false;
+
+let nextStepPromiseResolve = null;
+
+async function nextStepButtonClick() {
+  if (nextStepPromiseResolve !== null) {
+    nextStepPromiseResolve(); // Resolve the promise
+  }
+  return new Promise((resolve) => {
+    nextStepPromiseResolve = resolve;
+  });
+}
+
+async function startSearchButtonClick() {
+  const startNodeSelect = document.getElementById("startNodeSelect");
+  const goalNodeSelect = document.getElementById("goalNodeSelect");
+  const startSearchButton = document.getElementById("startSearchButton");
+  const nextStepButton = document.getElementById("nextStepButton");
+  startNodeSelect.disabled = true;
+  goalNodeSelect.disabled = true;
+  startSearchButton.disabled = true;
+  nextStepButton.disabled = false;
+
+  const path = await aStarSearch(startNodeSelect.value, goalNodeSelect.value);
+  if (!path) {
+    document.getElementById("resultDisplay").textContent =
+      "Search finished: No path found";
+  } else {
+    const length = path.slice(-1)[0].fScore.toFixed(1);
+    const pathAsString = path.map((node) => node.name).join(" ");
+    document.getElementById(
+      "resultDisplay"
+    ).textContent = `Search finished: Path found with length ${length} meters: ${pathAsString}`;
+  }
+
+  nextStepButton.disabled = true;
+}
+
+function toggleBackgroundButtonClick() {
+  DISPLAY_BACKGROUND = !DISPLAY_BACKGROUND;
+}
+
+function toggleFGPrevButtonClick() {
+  DISPLAY_F_G_PREV = !DISPLAY_F_G_PREV;
+}
+
+function toggleCoordinatesButtonClick() {
+  DISPLAY_COORDINATES = !DISPLAY_COORDINATES;
+}
+
+function toggleWeightsButtonClick() {
+  DISPLAY_WEIGHTS = !DISPLAY_WEIGHTS;
+}
