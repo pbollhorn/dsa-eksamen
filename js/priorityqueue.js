5339dbbb595fdc17e65027c9a8720131fc74f392
Peter Bollhorn <pbollhorn@gmail.com>
2025-12-07 17:38:58 +0100

    progression in PriorityQueue

--- js/priorityqueue.js
+++ js/priorityqueue.js
@@ -5,13 +5,22 @@ class PriorityQueue {
     this.#array = [];
   }
 
+  // enqueue node at the end
   enqueue(node) {
     this.#array.push(node);
   }
 
+  // dequeue node with lowest fScore
   dequeue() {
-    this.#array.sort((a, b) => a.fScore - b.fScore);
-    return this.#array.shift();
+  if (this.#array.length === 0) return undefined;
+
+  let lowestIndex = 0;
+  for (let i = 1; i < this.#array.length; i++) {
+    if (this.#array[i].fScore < this.#array[lowestIndex].fScore) {
+      lowestIndex = i;
+    }
+  }
+  return this.#array.splice(lowestIndex, 1)[0];
   }
 
   size() {
@@ -20,10 +29,6 @@ class PriorityQueue {
 
   includes(node) {
     return this.#array.includes(node);
-  }
-
-  print() {
-    console.log(this.#array);
   }
 
   toString() {
